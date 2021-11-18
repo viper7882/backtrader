5b5a7742d2cd6d7f6fffab35dea72bf4c0261647
viper7882 <klwhot7880@yahoo.co.uk>
2021-11-18 17:25:28 +0800

    Added test to confirm nested strategy is allowed to run.

---
+++ tests/test_nested_strategy.py
@@ -0,0 +1,37 @@
+import inspect
+
+import backtrader
+
+import testcommon
+
+
+class InnerStrategy(backtrader.Strategy):
+    def next(self):
+        print("{} Line: {}: ran {}".format(
+            inspect.getframeinfo(inspect.currentframe()).function,
+            inspect.getframeinfo(inspect.currentframe()).lineno,
+            InnerStrategy.__name__))
+        self.cerebro.runstop()
+
+
+class NestedStrategy(backtrader.Strategy):
+    def next(self):
+        datas = [testcommon.getdata(i) for i in range(1)]
+        testcommon.runtest(datas,
+                           InnerStrategy)
+
+        print("{} Line: {}: ran {}".format(
+            inspect.getframeinfo(inspect.currentframe()).function,
+            inspect.getframeinfo(inspect.currentframe()).lineno,
+            NestedStrategy.__name__))
+        self.cerebro.runstop()
+
+
+def test_run():
+    datas = [testcommon.getdata(i) for i in range(1)]
+    testcommon.runtest(datas,
+                       NestedStrategy)
+
+
+if __name__ == '__main__':
+    test_run()
